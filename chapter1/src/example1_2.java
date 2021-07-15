707c5945e2b8ee47954a991e0eb26fb5ea926a96
mouhatall77 <56361929+mouhatall77@users.noreply.github.com>
2021-07-14 22:42:20 -0400

    .

--- chapter1/src/example1_2.java
+++ chapter1/src/example1_2.java
@@ -1,5 +1,6 @@
 public class example1_2 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to Java, Learning JAva Now");
+        System.out.println("Programming is fun.");
     }
 }
