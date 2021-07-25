70287d3b78d96047c10f0c86ed8e515f39ebe692
mouhatall77 <56361929+mouhatall77@users.noreply.github.com>
2021-07-24 23:29:31 -0400

    .

--- chapter7/src/example7_3.java
+++ chapter7/src/example7_3.java
@@ -1,14 +1,49 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class example7_3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] number;
+        int[] number = createArray();
 
-        System.out.print("Enter the integers between 1 and 50: ");
-        while(input.nextInt() != 0) {
-            //number = new int[];
+        System.out.println(Arrays.toString(number));
+
+    }
+
+    //create and populate an array with user input
+    public static int[] createArray()
+    {
+        Scanner input = new Scanner(System.in);
+        int[] number = new int[50];
+        int count = 0;
+        System.out.println("Enter the integers between 1 and 50: ");
+        while (true)
+        {
+            int next = input.nextInt();
+            if(next == 0)
+            {
+                break;
+            }
+            else
+            {
+                number[count] = next;
+                count++;
+            }
         }
 
+        return number;
+    }
+
+    //Count occurences for each number
+    public static int[] countOccurence(int[] numbers)
+    {
+        int[] counts = new int[10];
+
+        for (int i=0; i< numbers.length; i++)
+        {
+            counts[numbers[i]]++;
+        }
+
+        return counts;
     }
 }
